ac17317861720e55f668db4438a887bb2c60e51f
KamihoriuchiYuki <0eh302b726001y@gmail.com>
2024-08-28 16:36:55 +0900

    delete

--- src/hand_sense_ws/src/subscriber/subscriber/open3d_test.py
+++
@@ -1,5 +0,0 @@
-import open3d #ライブラリのインポート
-
-pointcloud = open3d.io.read_point_cloud("bun.pcd") #点群ファイルを変数pointcloudに保存
-open3d.visualization.draw_geometries([pointcloud]) #点群を画像として表示
-open3d.io.write_point_cloud("output.pcd", pointcloud) #output.pcbという名前の点群ファイルを出力
