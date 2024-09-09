45e69e55117ec0243e9343097070b9218757a2ef
KamihoriuchiYuki <0eh302b726001y@gmail.com>
2024-09-09 16:19:58 +0900

    chenge

--- src/hand_sense_ws/src/subscriber/occlusion_index_learning/model_result.py
+++ src/hand_sense_ws/src/subscriber/occlusion_index_learning/model_result.py
@@ -159,7 +159,7 @@ class RsSub(Node):
             
             # 映像の保存設定
             fourcc = cv2.VideoWriter_fourcc(*'XVID')
-            self.video_out = cv2.VideoWriter(f"data_index/index_finger_video_{timestamp}.avi", fourcc, 16.06, (640, 480))
+            self.video_out = cv2.VideoWriter(f"data_index/index_finger_video_{timestamp}.avi", fourcc, 15.4942, (640, 480))
 
         else:
             self.f.close()
