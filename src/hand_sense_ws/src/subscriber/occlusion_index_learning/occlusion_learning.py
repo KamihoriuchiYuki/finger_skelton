43540967e3df751c43e371c36d05d930dc217fb1
KamihoriuchiYuki <0eh302b726001y@gmail.com>
2024-09-09 11:05:08 +0900

    model update

--- src/hand_sense_ws/src/subscriber/occlusion_index_learning/occlusion_learning.py
+++ src/hand_sense_ws/src/subscriber/occlusion_index_learning/occlusion_learning.py
@@ -42,7 +42,7 @@ def load_model(model_path):
     return joblib.load(model_path)
 
 # 学習データの準備
-file_path = 'learning_data/combined_data_20240830_194832.csv'
+file_path = 'learning_data/combined_data_20240830_195149.csv'
 data = load_data(file_path)
 X, y = prepare_data(data)
 
